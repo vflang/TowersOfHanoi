fc7075248f32d2578bc19c3f03623cc2249a23ce
Veronica Lang <TheSlug64@gmail.com>
2018-01-21 20:12:17 -0500

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,12 @@
+package com.company;
+/* Towers of Hanoi
+- goal is to print out solution/the steps
+*/
+public class Tester {
+
+    public static void main(String[] args) {
+        TowersOfHanoi eyah = new TowersOfHanoi();
+        eyah.TowersOfHanoi(3);
+        eyah.solveMethod(3);
+    }
+}
